636a59a192d6abd1d50104dedb86954f827dbf05
Martin Carnein <martin.carnein@mailbox.tu-dresden.de>
2026-02-05 23:34:16 +0100

    deleted unused folders

--- galois_testing/galois_field_test.py
+++
@@ -1,9 +0,0 @@
-import galois
-import numpy as np
-
-# this will be used to test the galois library for generating the orthogonality tags for our packets
-
-
-GF256 = galois.GF(2**8)
-print(GF256)
-
